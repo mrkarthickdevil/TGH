daa1db9079b02c5cffc21a2b70021df8cfb5ad1b
LinuxPoison <83256566+mrkarthickdevil@users.noreply.github.com>
2023-08-21 23:35:40 +0530

    Add files via upload
    TGH Telugu hacks group 
    From telegram : https://t.me/teluguhackersgroup1

---
+++ TGH.sh
@@ -0,0 +1,56 @@
+clear
+echo " opeaning  group  join now  " |lolcat
+echo ""
+echo "  support Group " |lolcat
+echo "" 
+xdg-open https://t.me/teluguhackersgroup1
+sleep 8
+clear
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№" |lolcat
+echo "" 
+echo ""
+echo " ████████╗░██████╗░██╗░░██╗ " |lolcat
+echo " ╚══██╔══╝██╔════╝░██║░░██║ " |lolcat
+echo " ░░░██║░░░██║░░██╗░███████║ " |lolcat
+echo " ░░░██║░░░██║░░╚██╗██╔══██║ " |lolcat
+echo " ░░░██║░░░╚██████╔╝██║░░██║ " |lolcat 
+echo " ░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝ " |lolcat 
+echo ""
+echo "  TGH full form Telugu_Hacks_group " |lolcat
+echo ""
+echo " join telegram group @Offical_Telugu_Hacks " |lolcat 
+echo ""
+echo ""
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№" |lolcat
+echo ""
+echo "" 
+sleep 1
+while :
+do
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "          TGH full form Telugu_Hacks_group " |lolcat
+echo ""
+echo ""
+echo ""
+echo "         ████████╗░██████╗░██╗░░██╗ " |lolcat
+echo "         ╚══██╔══╝██╔════╝░██║░░██║ " |lolcat
+echo "         ░░░██║░░░██║░░██╗░███████║ " |lolcat
+echo "         ░░░██║░░░██║░░╚██╗██╔══██║ " |lolcat
+echo "         ░░░██║░░░╚██████╔╝██║░░██║ " |lolcat
+echo "         ░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝ " |lolcat
+echo ""
+echo ""
+echo "         TGH full form Telugu_Hacks_group " |lolcat
+echo ""
+sleep 1
+echo ""
+echo ""
+echo ""
+echo ""
+
+clear
+done
